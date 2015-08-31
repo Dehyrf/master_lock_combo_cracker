4d881dc8a28ea269852b395db82475109bb2edd8
Dehyrf <Dehyrf@users.noreply.github.com>
2015-08-30 20:24:50 -0500

    On branch master  Your branch is up-to-date with 'origin/master'.
     Changes to be committed:
    	modified:   master_cracker/master_cracker.py

--- master_cracker/master_cracker.py
+++ master_cracker/master_cracker.py
@@ -1,5 +1,7 @@
 import math
+print
 print 'For tutorial, please see video: https://www.youtube.com/watch?v=09UgmwtL12c.'
+print
 firstLocked = int(raw_input('First locked position: '))
 secondLocked = int(raw_input('Second locked position: '))
 
@@ -14,17 +16,25 @@ third = []
 second = []
 for x in range(0, 3):
 	if (((10 * x) + firstLocked) % 4 == mod):
-		third.append((10 * x) + firstLocked)	
+		third.append(int((10 * x) + firstLocked))	
 	if (((10 * x) + secondLocked) % 4 == mod):
-		third.append((10 * x) + secondLocked)		
-print 'Possible third digits: ' 
-print third
+		third.append(int((10 * x) + secondLocked))		
+print
+print 'Possible third digits: ' + str(third)[1:-1]
 thirdVal = int(raw_input('Which is the third digit? '))
-for x in range(0, 9):
+if(thirdVal != third[0] and thirdVal != third[1]):
+	print 'Error, incorrect value!'
+	exit()
+for x in range(0, 10):
 	temp = ((mod + 2) % 4) + (4 * x)
 	if((thirdVal + 2) % 40 != temp and (thirdVal - 2) % 40 != temp):
-		second.append(temp)
-print firstVal
+		second.append(int(temp))
+firstVal = int(firstVal)
+print
+print
+print
+print 'First number: ' + str(firstVal)
+print 'Possible second numbers: '
 print str(second)[1:-1]
-print second
-print thirdVal
+print 'Third number: ' + str(thirdVal)
+print
